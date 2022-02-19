d43fe2fde4721761b4764842c53d3138a27f313c
Nickolay Mateshko <nickolay@padsplit.com>
2022-02-19 12:09:54 +0300

    Add initial migrations

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Faculty(models.Model):
+    full_name = models.CharField(max_length=120)
+    short_name = models.CharField(max_length=120)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=120, unique=True)
+    password = models.CharField(max_length=250)
+    first_name = models.CharField(max_length=120, null=True)
+    last_name = models.CharField(max_length=120, null=True)
+    avatar_url = models.ImageField(null=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    USERNAME_FIELD = 'username'
+
+
