25ac0a801fd9d4c254d51dc6affad8839f80d5f5
ZinganDovgaluk <SoftMark228@gmail.com>
2022-02-19 12:27:01 +0200

    add all models

--- forum/admin.py
+++ forum/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from forum.models import User, Faculty, UserProfile
-
+from forum.models import User, UserProfile, Faculty, Post, PostCategory, PostReaction, PostComment, PostCommentReaction
 
 admin.site.register(User)
 admin.site.register(UserProfile)
 admin.site.register(Faculty)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(PostReaction)
+admin.site.register(PostComment)
+admin.site.register(PostCommentReaction)
